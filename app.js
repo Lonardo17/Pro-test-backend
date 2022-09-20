ad013bf2a7a624414c7b4539c8f3b0d3c8874e58
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-09-20 22:34:44 +0300

    end-test

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-// const contactsRouter = require('./routes/api/contacts')
-
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -11,14 +9,6 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-
-// app.use('/api/contacts', contactsRouter)
-app.get('/', (req, res) => {
-  res.end('<h1>home page<h1>')
-})
-app.get('/about', (req, res) => {
-  res.end('<h1>about page<h1>')
-})
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
