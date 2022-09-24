cfe988944293c02b95e4b6eab5a8536744ea8376
Nadia Khoptiuk <holineinadia@gmail.com>
2022-09-24 14:05:26 +0300

    change method at route /test

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,13 +1,14 @@
 function validate(schema) {
   return (req, res, next) => {
+    console.log(req.body);
     const result = schema.validate(req.body);
 
     if (result.error) {
       const errorField = result.error.details[0].context.key;
 
-      return res
-        .status(400)
-        .json({ message: `missing value or error at ${errorField} field` });
+      return res.status(400).json({
+        message: `missing value or error at ${errorField} field`,
+      });
     }
 
     req.body = result.value;
