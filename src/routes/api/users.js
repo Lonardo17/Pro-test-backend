e63dce7cd4b0b46321ee32b0e313dda53fa20f1e
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-10-03 18:40:47 +0300

    deleted

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -6,10 +6,8 @@ const {
   getCurrentUser,
   loginGoogleUsers,
 } = require("../../controllers/controllerUsers");
-const { googleAuth } = require("../../controllers/googleAuth");
 const { userValidate } = require("../../middlewares/validateUsers");
 const { authMiddleware } = require("../../middlewares/authMiddleware");
-const authenticateGogle = require("../../middlewares/auhenticate-social");
 
 const router = express.Router();
 
@@ -18,15 +16,5 @@ router.post("/authorization", userValidate, userLogin);
 router.get("/logout", authMiddleware, userLogout);
 router.get("/current", authMiddleware, getCurrentUser);
 router.post("/googlelogin", loginGoogleUsers);
-
-router.get(
-  "/google",
-  authenticateGogle.authenticate("google", { scope: ["email", "profile"] })
-);
-router.get(
-  "/google/callback",
-  authenticateGogle.authenticate("google", { successRedirect: "/auth" }),
-  googleAuth
-);
 
 module.exports = router;
